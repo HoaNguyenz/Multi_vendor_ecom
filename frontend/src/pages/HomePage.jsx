6f4cdcecd9b9b7ca53404cf7a151d21f125807d9
VietAnh1803 <anh.nguyen18032004@hcmut.edu.vn>
2024-11-09 23:29:18 +0700

    Thêm HomePage and Header components

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import Header from "../components/Layout/Header";
+import Hero from "../components/Route/Hero/Hero.jsx";
+//import Categories from "../components/Route/Categories/Categories";
+//import BestDeals from "../components/Route/BestDeals/BestDeals";
+//import FeaturedProduct from "../components/Route/FeaturedProduct/FeaturedProduct";
+//import Events from "../components/Events/Events";
+//import Sponsored from "../components/Route/Sponsored";
+//import Footer from "../components/Layout/Footer";
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div>
+        <Header activeHeading={1} />
+        <Hero />
+    </div>
   )
 }
 
