39793f56334671f8a8159d7d4f4778563327d666
VietAnh1803 <anh.nguyen18032004@hcmut.edu.vn>
2024-11-20 18:17:54 +0700

    Cập nhật header cho trang homepage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Header from "../components/Layout/Header.jsx"
+import Categories from "../components/Route/Categories/Categories.jsx"
+import BestDeals from "../components/Route/BestDeals/BestDeals.jsx"
 
 const HomePage = () => {
   return (
     <div>
       <Header></Header>
+      <Categories/>
+      <BestDeals/>
     </div>
   )
 }
